4781a3f857b5fc524563fb613a3327ded225886c
Haira Lei <darhaii13@gmail.com>
2022-11-11 21:33:06 +0800

    fix bug about unique

--- models/User.js
+++ models/User.js
@@ -14,8 +14,8 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please provide valid email',
     ],
+    unique: true,
   },
-  unique: true,
   password: {
     type: String,
     required: [true, 'Please provide password'],
