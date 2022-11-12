06955f779756fb411c63108a9b8ac275fa92d624
Haira Lei <darhaii13@gmail.com>
2022-11-12 23:08:49 +0800

    modify create jobs function

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -17,6 +17,7 @@ const auth = async (req, res, next) => {
 
     // *attach user to job routes
     req.user = { userId: payload.userId, name: payload.name };
+    next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid');
   }
