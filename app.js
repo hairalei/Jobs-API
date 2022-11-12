9992ec2d3e67abce27460ad697c646b41fb31600
Haira Lei <darhaii13@gmail.com>
2022-11-12 21:46:29 +0800

    add compare password funciton

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
-// app.use(errorHandlerMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
