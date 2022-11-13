a425faa6bfe85c13f757db6612e339bb4e7e64dd
Haira Lei <darhaii13@gmail.com>
2022-11-13 15:42:28 +0800

    refactor custom error and add validation error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,31 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+const { CustomAPIError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    // *set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong. Please try again.',
+  };
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `${Object.keys(err.keyValue)} already exists`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+};
+
+module.exports = errorHandlerMiddleware;
