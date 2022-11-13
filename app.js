3951d544b675cdcfb941d5b913f645c877b9ff08
Haira Lei <darhaii13@gmail.com>
2022-11-13 19:41:01 +0800

    fix bug regarding ratelimiter

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use('trust proxy', 1);
+app.set('trust proxy', 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
